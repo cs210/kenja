e526d79163b9fb37cfda1312c47e629233c7b841
cpondoc <chris.pondoc@comcast.net>
2024-02-18 23:34:33 -0800

    Fix sqllite issue?

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+"""
+Main interface for the Sommelier app.
+"""
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from sommelier_functions.sommelier_helpers import *
 
