66bea88e137fd50dfa31faa0480f16e9439e67e9
joey-obrien <joeyob7@gmail.com>
2024-02-20 14:26:18 -0800

    baseline MVP for book recommendations

---
+++ book_app.py
@@ -0,0 +1,45 @@
+"""
+Main interface for the Sommelier app.
+"""
+# Stuff for Streamlit -- comment out for now
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+# Main packages to import
+import streamlit as st
+from book_rec_helpers import *
+from metrics import SEARCH_COUNT
+from state import count_sessions
+
+# Header + increment visit count
+count_sessions()
+st.title("📚 Book Finder")
+st.subheader("You give us a request, we give you book recs.")
+
+# Core form/search function
+submitted = False
+slider_val = ""
+with st.form("input_form"):
+   st.write("Describe what type of book you're looking for...")
+   slider_val = st.text_input("Ex: \"I want a book that is about magic.\"")
+
+   # Submit function
+   submitted = st.form_submit_button("Submit")
+
+# Checking for submitted + increment count
+if submitted:
+    SEARCH_COUNT.inc()
+    st.subheader("Recommendations")
+    results = find_match(slider_val)
+
+    # Display results
+    for i in range(len(results['documents'][0])):
+        metadata = results['metadatas'][0][i]
+        st.write("📘 " + metadata['title'])
+        st.write("Description: " + metadata['description'])
+        st.write("score: " + str(metadata['score']))
+        st.write("publisher: " + metadata['publisher'])
+        st.write("publication date: " + metadata['publisher'])
+        st.write("Link to buy: " + metadata['link'])
+        st.divider()
--- book_rec_helpers.py
+++ book_rec_helpers.py
@@ -4,9 +4,9 @@ import csv
 from dotenv import load_dotenv
 from openai import OpenAI
 
-# load_dotenv()
-# chroma_client = chromadb.Client(Settings(anonymized_telemetry=False))
-# client = OpenAI()
+load_dotenv()
+chroma_client = chromadb.Client(Settings(anonymized_telemetry=False))
+client = OpenAI()
 
 # Constants for data
 CONFIG_FILE = "config.json"
@@ -15,7 +15,14 @@ BOOKS_DATA_PATH = "archive/books_data.csv"
 EMBEDDING_MODEL = "text-embedding-3-small"
 
 """
-- only 26,522 books considered right now
+Extra TODOs:
+- filter out low rated books (use google or amazon review values)
+- only use reviews from amazon that give a rating of 3 or above
+"""
+
+
+"""
+- only 5796 books considered right now
 
 *** Notes *** : 
 - there are books that have isbn values but no title --> make sure to ignore these
@@ -31,32 +38,58 @@ EMBEDDING_MODEL = "text-embedding-3-small"
       the same and just pick one of the isbn values
     - note that we will assume that no two books can have the same title (not sure if this is true, can
       only be a problem if the 3237 books above are actually different)
+      - upon further investigation, popular books like harry potter are being tagged with these non-isbn ids
+       these ids are the Amazon product numbers, or ASIN
 - we will only consider books that have all values filled out in books_data.csv --> reduced number to *** 40635 *** books
 - Adding the restriction of only considering isbns --> number of books is reduced to 26522
     - 832 books have multiple isbns, however, so the true value is 25,690
-"""
+- upon further reflection, we have removed ratings count as a requried value for google, but will be keeping description
 
+New notes:
+- Ok, so it seems like books that do not have an ASIN and are being tracked with their isbns are actually the more obscure books,
+  so switched to including ASIN values and got a TON more options (when using isbn --> none of the books had more than 1 review. Now,
+  more than 60,000 books have 5 or more reviews)
+- probably want to filter on genre more specifically at some point (only filtering out books with no genre label right now)
+- Note: these values are for not fitlering out reviews with a rating less than 3:
+    - at least 10 reviews: 33291
+    - at least 15 reviews: 22880
+    - at least 20 reviews: 17321
+    - at least 25 reviews: 13760
+- Note: filtering out reviews that are less than 4
+    - at least 25 reviews: 10832
+- Filtering out reviews that are less than 3:
+    - at least 25 reviews: 12,077
+- Filtering out reviews that are less than 4 and books with an average review rating less than 4:
+    - at least 25 reviews: 8651
+- Filtering out reviews that are less than 4 and books with an average review rating less than 4.2:
+    - at least 30 reviews: 5796 books
+- Need to make sure to filter out bad books later (my assumption right now, though, is that books with many reviews will have ok ratings)
+ - will just remove reviews that are less than 3 for now as to not confuse the model
+"""
 class BookInfo:
     """
-    Class for wine objects
+    Class for book objects
     """
     def __init__(self, row):
+        # books data csv data
+        self.title = row[0]
+        self.description = row[1]
+        self.authors = row[2]
+        self.book_cover_url = row[3]
+        self.google_preview_link = row[4]
+        self.publisher = row[5]
+        self.publish_date = row[6]
+        self.google_info_link = row[7] # Note: I am not sure how this is different from the google preview link
+        self.category = row[8]
+        # self.average_google_rating = row[9] I'm not actually sure what this value means, as it goes up to 4,900
 
-
-        """
-        self.raw_content = row[1:] # the id is the key in the hashmap
-        self.amazon_link = "http://www.amazon.com/dp/" + str(row[0])
-        self.title = row[1]
-        # initialize a list of all the review ratings, summaries, and the reviews themselves
-        self.book_score = [float(row[6])]
-        self.review_summaries = [row[8]]
-        self.reviews = [row[9]] 
-        # row[2] is price --> intentionally not including for now
-        # row[3] is user id --> not wanted information
-        # row[4] is profile name --> not wanted information
-        # row[5] is helpful rating of the review --> confused about how this is structured in the dataset, maybe use later
-        # row[7] is review time --> not wanted information
-        """
+        # books rating csv data
+        self.id = None
+        self.amazon_link = None
+        self.amazon_average_book_score = [] # this starts as a list of all the review scores, and then we take the average at the end
+        self.review_summaries = []
+        self.reviews = []
+        # Note: could also look at price later
 
 def load_data():
     """
@@ -69,69 +102,58 @@ def load_data():
         data_reader = csv.reader(data_file)
         start = 1
 
-        # we already know every row has a unique value here
+        # we already know every row has a unique value in this csv
         for row in data_reader:
             if start:
                 start = 0
                 continue
             skip = 0
-            for i in range(len(row)):
-                if row[i] == "":
-                    skip = 1
-                    break
+            # if the description for this book is not filled out, skip this book
+            if row[1] == "" or row[8] == "":
+                skip = 1
             if skip:
                 continue
-            all_books_dict[row[0]] = 0
 
-    all_isbns_dict = {}
+            # clean up the list of authors and the cetegory (only 1 category ever listed)
+            row[2] = row[2][1:len(row[2])-1]
+            row[2] = row[2].replace("'", "")
+            row[8] = row[8][1:len(row[8])-1]
+            row[8] = row[8].replace("'", "")
 
-    with open(BOOKS_RATING_PATH, mode='r') as file:
-        reader = csv.reader(file)
-        
-        index = 0
-        for row in reader:
-            if index == 0:
-                index += 1
+            all_books_dict[row[0]] = BookInfo(row)
+
+    with open(BOOKS_RATING_PATH, mode='r') as rating_file:
+        rating_reader = csv.reader(rating_file)
+        start = 1
+        for row in rating_reader:
+            if start:
+                start = 0
                 continue
-            # if row[1] in all_titles_dict and row[0] not in all_books_dict:
-            #     print(row[1])
-        
-
-            # if row[0] not in all_books_dict and row[0][0].isnumeric():
-            #     all_books_dict[row[0]] = 1
-            #     if row[1] not in all_titles_dict:
-            #         all_titles_dict[row[1]] = 1
-            #     else:
-            #         num_same_title_different_isbn += 1
 
             if row[1] in all_books_dict:
-                if row[0] not in all_isbns_dict and row[0][0].isnumeric():
-                    all_isbns_dict[row[0]] = 1
-                    all_books_dict[row[1]] += 1
-    total_repeated = 0
+                # Note that for books that have multiple ids, we just pick one for now
+                # also filter out reviews that gave a rating of less than 3 (don't want to overly confuse the model)
+                current_book = all_books_dict[row[1]]
+                if row[9] != "":
+                    if current_book.id is None:
+                        current_book.id = row[0]
+                        current_book.amazon_link = "http://www.amazon.com/dp/" + str(row[0])
+                    current_book.amazon_average_book_score.append(float(row[6]))
+                    if float(row[6]) >= 4:
+                        current_book.review_summaries.append(row[8])
+                        current_book.reviews.append(row[9])
+    
+    all_books_list = []
+
     for key in all_books_dict:
-        if all_books_dict[key] > 1:
-            total_repeated += 1
-    print(total_repeated)
-    print(len(all_isbns_dict.keys()))
-    print(len(all_books_dict.keys()))
-    # print(len(all_books_dict.keys()))
-    # print(len(all_books_dict.keys()) - num_here)
-    #         if row[0] not in all_books_dict:
-    #             all_books_dict[row[0]] = BookInfo(row)
-    #         else:
-    #             all_books_dict[row[0]].book_score.append(float(row[6]))
-    #             all_books_dict[row[0]].review_summaries.append(row[8])
-    #             all_books_dict[row[0]].reviews.append(row[9])
-    #         if index == 1000:
-    #             break
-    #         index += 1
-    # all_books_list = []
-    # for key in all_books_dict:
-    #     all_books_dict[key].isbn = key
-    #     all_books_dict[key].book_score = str(sum(all_books_dict[key].book_score) / len(all_books_dict[key].book_score))
-    #     all_books_list.append(all_books_dict[key])
-    # return all_books_list
+        current_book = all_books_dict[key]
+        # set average review value
+        current_book.amazon_average_book_score = sum(current_book.amazon_average_book_score) / len(current_book.amazon_average_book_score)
+        # we want at least 30 reviews for a book
+        if len(current_book.reviews) < 30 or current_book.amazon_average_book_score < 4.2:
+            continue
+        all_books_list.append(current_book)
+    return all_books_list
 
 def create_embedding(description):
     """
@@ -139,20 +161,26 @@ def create_embedding(description):
     """
     return client.embeddings.create(input = [description], model=EMBEDDING_MODEL).data[0].embedding
 
-def add_wines(collection, book):
+def add_books(collection, book):
     """
-    Add wine to our vector database.
+    Add book to our vector database.
     """
+    # do some prompt engineering on the input --> we will pass in the book description and 3 reviews to start
+    # we know that these reviews are associated with ratings of at least 4
+    book_info = 'Title: ' + book.title + '\n' + "Description: " + book.description + "\n"
+    for i in range(1,4,1):
+        book_info += "Review " + str(i) + ":" + book.reviews[i] + "\n"
+
     collection.add(
-        embeddings=[create_embedding(wine.description)],
-        documents=[wine.description],
-        metadatas=[{"designation": wine.designation, "variety": wine.variety, "winery": wine.winery}],
-        ids=[book.isbn]
+        embeddings=[create_embedding(book_info)],
+        documents=[book_info],
+        metadatas=[{"title": book.title, "description": book.description, "category": book.category, "score": book.amazon_average_book_score, "link": book.amazon_link, "publisher": book.publisher, "publication_date": book.publish_date}],
+        ids=[book.id]
     )
 
-def find_wine(collection, query):
+def find_book(collection, query):
     """
-    Find a similar wine based on our query.
+    Find a similar book based on our query.
     """
     embedding = create_embedding(query)
     results = collection.query(
@@ -169,8 +197,8 @@ def find_match(query):
     books = load_data()
     collection = chroma_client.create_collection(name="books")
     for i in range(25):
-        add_wines(collection, books[i])
+        add_books(collection, books[i])
 
     # Find a similar wine
-    results = find_wine(collection, query)
+    results = find_book(collection, query)
     return results
