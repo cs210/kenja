af317a1c6a9a40b838402a9e72b90db856faab9f
travissenf <tsenf@stanford.edu>
2024-02-21 11:09:48 -0800

    Committing changes that update the collection for wines, give potential functionality for mapping location to an actual map, and adds more information for wines in output.

--- old/sommelier/app.py
+++ old/sommelier/app.py
@@ -1,16 +1,22 @@
 """
 Main interface for the Sommelier app.
 """
+
 # Stuff for Streamlit -- comment out for now
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 # Main packages to import
 import streamlit as st
-from sommelier_functions.sommelier_helpers import *
-from helpers.metrics import SEARCH_COUNT
-from helpers.state import count_sessions
+import folium
+from geopy.geocoders import Nominatim
+from sommelier_functions.sommelier_helpers import find_match
+from metrics import SEARCH_COUNT
+from state import count_sessions
+
+# Initialize geopy geocoder
+geolocator = Nominatim(user_agent="geoapiExercises", timeout=10)
 
 # Header + increment visit count
 count_sessions()
@@ -21,11 +27,13 @@ st.subheader("You give us a request, we give you wine recs.")
 submitted = False
 slider_val = ""
 with st.form("input_form"):
-   st.write("Describe what wine you're looking for...")
-   slider_val = st.text_input("Ex: \"I want a wine that goes well with gouda and has notes of paprika.\"")
+    st.write("Describe what wine you're looking for...")
+    slider_val = st.text_input(
+        'Ex: "I want a wine that goes well' ' with gouda and has notes of paprika."'
+    )
 
-   # Submit function
-   submitted = st.form_submit_button("Submit")
+    # Submit function
+    submitted = st.form_submit_button("Submit")
 
 # Checking for submitted + increment count
 if submitted:
@@ -34,9 +42,36 @@ if submitted:
     results = find_match(slider_val)
 
     # Display results
-    for i in range(len(results['documents'][0])):
-        metadata = results['metadatas'][0][i]
-        st.write("🍷 " + metadata['designation'])
-        st.write("Vineyard: " + metadata['winery'])
-        st.write("Variety: " + metadata['variety'])
+    for i in range(len(results["documents"][0])):
+        metadata = results["metadatas"][0][i]
+        st.write("🍷 " + metadata["designation"])
+        st.write("Vineyard: " + metadata["winery"])
+        st.write("Variety: " + metadata["variety"])
+        st.write("Points: " + metadata["points"])
+        st.write("Country: " + metadata["country"])
+        st.write(
+            "Location: "
+            + metadata["province"]
+            + ", "
+            + metadata["region_1"]
+            + ", "
+            + metadata["region_2"]
+        )
+        st.write("Price: " + metadata["price"])
+
+        try:
+            loc = geolocator.geocode(
+                metadata["province"] + ", " + metadata["region_1"], timeout=10
+            )
+            if loc:
+                m = folium.Map(location=[loc.latitude, loc.longitude], zoom_start=10)
+                folium.Marker(
+                    [loc.latitude, loc.longitude], popup=metadata["winery"]
+                ).add_to(m)
+                st.write(m)
+            else:
+                st.write("Could not find location")
+
+        except Exception as e:
+            print("An error occurred:", e)
         st.divider()
--- old/sommelier/sommelier_functions/sommelier_helpers.py
+++ old/sommelier/sommelier_functions/sommelier_helpers.py
@@ -5,13 +5,16 @@ from dotenv import load_dotenv
 from openai import OpenAI
 
 load_dotenv()
-chroma_client = chromadb.PersistentClient(path="./chromadb_data", settings=Settings(anonymized_telemetry=False))
+chroma_client = chromadb.PersistentClient(path="./chromadb_data",
+                                          settings=Settings(
+                                              anonymized_telemetry=False))
 client = OpenAI()
 
 # Constants for data
 CONFIG_FILE = "config.json"
 DATA_PATH = "data/initial_wine_data.csv"
 EMBEDDING_MODEL = "text-embedding-3-small"
+
 
 class WineInfo:
     """
@@ -31,25 +34,28 @@ class WineInfo:
         self.variety = row[9]
         self.winery = row[10]
 
+
 def load_data():
     """
     Load data: currently in CSV file.
     """
     # Open the CSV file
     all_wines = []
-    with open(DATA_PATH, mode='r') as file:
+    with open(DATA_PATH, mode='r', encoding='utf-8') as file:
         reader = csv.reader(file)
-        
         # Create a wine info object
         for row in reader:
             all_wines.append(WineInfo(row))
     return all_wines[1:]
 
+
 def create_embedding(description):
     """
     Given a description, return an embedding.
     """
-    return client.embeddings.create(input = [description], model=EMBEDDING_MODEL).data[0].embedding
+    return client.embeddings.create(input=[description],
+                                    model=EMBEDDING_MODEL).data[0].embedding
+
 
 def add_wines(collection, wine):
     """
@@ -58,9 +64,14 @@ def add_wines(collection, wine):
     collection.add(
         embeddings=[create_embedding(wine.description)],
         documents=[wine.description],
-        metadatas=[{"designation": wine.designation, "variety": wine.variety, "winery": wine.winery}],
+        metadatas=[{"designation": wine.designation, "variety": wine.variety,
+                    "winery": wine.winery, "country": wine.country,
+                    "points": wine.points, "price": wine.price,
+                    "province": wine.province, "region_1": wine.region_1,
+                    "region_2": wine.region_2}],
         ids=[wine.id]
     )
+
 
 def find_wine(collection, query):
     """
@@ -72,6 +83,7 @@ def find_wine(collection, query):
         n_results=3
     )
     return results
+
 
 def find_match(query):
     """
@@ -89,6 +101,7 @@ def find_match(query):
     else:
         print("Populating data from existing chromadb collection")
 
+    print(collection)
 
     # Find a similar wine
     results = find_wine(collection, query)
