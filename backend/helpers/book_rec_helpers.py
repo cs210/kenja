070676a57378e4591fc770b7d5ef09b2052a46a7
joey-obrien <joeyob7@gmail.com>
2024-03-10 19:15:09 -0700

    completed first and second levels of FandF

--- backend/helpers/book_rec_helpers.py
+++ backend/helpers/book_rec_helpers.py
@@ -1,8 +1,10 @@
+from venv import create
 import chromadb
 from chromadb.config import Settings
 import pickle
 from dotenv import load_dotenv
 from openai import OpenAI
+import pandas as pd
 
 load_dotenv()
 chroma_client = chromadb.PersistentClient(
@@ -13,38 +15,8 @@ client = OpenAI()
 # Constants for data
 CONFIG_FILE = "config.json"
 EMBEDDING_MODEL = "text-embedding-3-small"
-DATASET_PATH = "./defined_datasets/5782_books_30_min_reviews_4.2_min_rating_50_min_review_length.pkl"
-
-
-class BookInfo:
-    """
-    Class for book objects
-    """
-
-    def __init__(self, row):
-        # books data csv data
-        self.title = row[0]
-        self.description = row[1]
-        self.authors = row[2]
-        self.book_cover_url = row[3]
-        self.google_preview_link = row[4]
-        self.publisher = row[5]
-        self.publish_date = row[6]
-        self.google_info_link = row[
-            7
-        ]  # Note: I am not sure how this is different from the google preview link
-        self.category = row[8]
-        # self.average_google_rating = row[9] I'm not actually sure what this value means, as it goes up to 4,900
-
-        # Note: could also look at price later
-        # books rating csv data
-        self.id = None
-        self.amazon_link = None
-        self.amazon_average_book_score = (
-            []
-        )  # this starts as a list of all the review scores, and then we take the average at the end
-        self.review_summaries = []
-        self.reviews = []
+BOOKS_DATASET_PATH = "./defined_datasets/books_df_5776_books_30_min_num_reviews_4.2_min_rating_20_min_description_length_50_min_review_length.csv"
+REVIEWS_DATASET_PATH = "./defined_datasets/reviews_df_633987_total_reviews_30_min_num_reviews_4.2_min_rating_20_min_description_length_50_min_review_length.csv"
 
 def create_embedding(text):
     """
@@ -56,27 +28,15 @@ def create_embedding(text):
         .embedding
     )
 
-def add_to_collection(collection, input, id, book):
+def add_to_collection(collection, embeddings, documents, metadatas, ids):
     """
     Add an embedding to the appropriate vector database.
     """
     collection.add(
-        embeddings=[create_embedding(input)],
-        documents=[input],
-        metadatas=[
-            {
-                "title": book.title,
-                "description": book.description,
-                "category": book.category,
-                "score": book.amazon_average_book_score,
-                "link": book.amazon_link,
-                "publisher": book.publisher,
-                "publication_date": book.publish_date,
-                "length": len(input),
-                "authors": book.authors
-            }
-        ],
-        ids=[id],
+        embeddings=embeddings,
+        documents=documents,
+        metadatas=metadatas,
+        ids=ids,
     )
 
 def embedding_search(collection, query, n_results):
@@ -84,39 +44,93 @@ def embedding_search(collection, query, n_results):
     results = collection.query(query_embeddings=embedding, n_results=n_results)
     return results
 
+
 def find_match(query, update_collections = False):
     """
     Call all functions.
     """
     descriptions_collection = chroma_client.get_or_create_collection(name="book_descriptions")
     reviews_collection = chroma_client.get_or_create_collection(name="book_reviews")
+    middle_collection = chroma_client.get_or_create_collection(name="middle_collection")
     if update_collections:
         chroma_client.delete_collection(name="book_descriptions")
         chroma_client.delete_collection(name="book_reviews")
+        chroma_client.delete_collection(name="middle_collection")
         descriptions_collection = chroma_client.create_collection(name="book_descriptions")
         reviews_collection = chroma_client.create_collection(name="book_reviews")
+        middle_collection = chroma_client.create_collection(name="middle_collection")
 
     if reviews_collection.count() == 0:
         print("populating data from file")
-        with open(DATASET_PATH, "rb") as f:
-            books = pickle.load(f)
-            index = 0
-            for book in books:
-                if index == 500:
-                    break
-                print(index)
-                for i in range(len(book.reviews)):
-                    if i == 10:
-                        break
-                    current_id = f"{book.id,i}"
-                    add_to_collection(reviews_collection, book.reviews[i], current_id, book)
-                add_to_collection(descriptions_collection, book.description , book.id , book)
-                index += 1
+
+        # FIRST LEVEL
+        books_df = pd.read_csv(BOOKS_DATASET_PATH, nrows=5)
+
+        reviews_df = pd.read_csv(REVIEWS_DATASET_PATH)
+        reviews_df = reviews_df[reviews_df['Title'].isin(books_df['Title'])]
+
+        reviews_df["helpful_votes"] = reviews_df["review/helpfulness"].apply(lambda x: str(x)[:x.find("/")])
+        reviews_df["helpful_votes"] = pd.to_numeric(reviews_df["helpful_votes"])
+
+        # only take the top 10 reviews for each book based on the number of helpful upvotes for now
+        top_reviews_df = reviews_df.groupby("Title").apply(lambda x: x.nlargest(10, "helpful_votes")).reset_index(drop=True)
+
+        books_metadatas = books_df.to_dict(orient="records")
+        reviews_metadatas = top_reviews_df.to_dict(orient="records")
+        
+        books_df["description_embedding"] = books_df["description"].apply(lambda x: create_embedding(x))
+        top_reviews_df["review_embedding"] = top_reviews_df["review/text"].apply(lambda x: create_embedding(x))
+
+        books_embeddings = books_df["description_embedding"].tolist()
+        reviews_embeddings = top_reviews_df["review_embedding"].tolist()
+
+        books_ids = books_df["Title"].tolist()
+        reviews_ids = top_reviews_df["reviewId"].tolist()
+
+        books_documents = books_df["description"].tolist()
+        reviews_documents = top_reviews_df["review/text"].to_list()
+
+        add_to_collection(reviews_collection, reviews_embeddings, reviews_documents, reviews_metadatas, reviews_ids)
+        add_to_collection(descriptions_collection, books_embeddings, books_documents, books_metadatas, books_ids)
+
+        # SECOND LEVEL
+        exclude = ["description_embedding", "description_length"]
+        middle_df = books_df[[col for col in books_df.columns if col not in exclude]]
+        middle_df["description"] = "Description: " + middle_df["description"] + "\n\n"
+
+        top_reviews_df = reviews_df.groupby("Title").apply(lambda x: x.nlargest(4, "helpful_votes")).reset_index(drop=True)
+
+        top_reviews_df = top_reviews_df[["Title", "review/text"]]
+        top_reviews_df["review/text"] = "Review: " + top_reviews_df["review/text"]
+        top_reviews_df = top_reviews_df.groupby("Title").agg({'review/text': lambda x: '\n\n'.join(map(str, x))}).reset_index()
+
+        middle_df = pd.merge(middle_df, top_reviews_df, on="Title", how="inner")
+        
+        middle_df["combined_text"] = middle_df["description"] + middle_df["review/text"]
+        middle_df = middle_df.drop("description", axis=1)
+        middle_df = middle_df.drop("review/text", axis=1)
+        middle_df["text_length"] = middle_df["combined_text"].apply(lambda x: len(str(x)))
+
+        middle_metadatas = middle_df.to_dict(orient="records")
+        middle_df["combined_text_embedding"] = middle_df["combined_text"].apply(lambda x: create_embedding(x))
+        middle_embeddings = middle_df["combined_text_embedding"].tolist()
+        middle_ids = middle_df["Title"].tolist()
+        middle_documents = middle_df["combined_text"].to_list()
+        add_to_collection(middle_collection, middle_embeddings, middle_documents, middle_metadatas, middle_ids)
     else:
         print("Populating data from existing chromadb collection")
-    print("here0!")
-    review_search_results = embedding_search(reviews_collection, query, 200)
-    print("here1!")
-    description_search_results = embedding_search(descriptions_collection, query, 20)
-    print("here2!")
-    # return description_results
+    review_search_results = embedding_search(reviews_collection, query, 2)
+    titles_list = [dictionary["Title"] for dictionary in review_search_results["metadatas"][0]]
+    titles_list = list(set(titles_list)) # there is a chance that the same book will appear multiple times
+    description_search_results = embedding_search(descriptions_collection, query, 2)
+    titles_list.extend(description_search_results["ids"][0])
+    titles_list = list(set(titles_list)) # same book could have been given by both reviews and descriptions
+
+    # SECOND LEVEL
+    extract_dict = middle_collection.get(ids=titles_list, include=["embeddings", "documents", "metadatas"])
+    # probably want to be more clever with this name
+    extract_collection = chroma_client.create_collection(name=query)
+    add_to_collection(extract_collection, extract_dict["embeddings"], extract_dict["documents"], extract_dict["metadatas"], extract_dict["ids"])
+    middle_search_results = embedding_search(extract_collection, query, 1)
+    chroma_client.delete_collection(name=query)
+    print(middle_search_results)
--- backend/helpers/refine_books_dataset.py
+++ backend/helpers/refine_books_dataset.py
@@ -8,7 +8,7 @@ import uuid
 BOOKS_DATA_PATH = "archive/books_data.csv"
 BOOKS_RATING_PATH = "archive/books_rating.csv"
 
-def load_kaggle_data(num_reviews, min_rating, min_review_length, min_description_length):
+def load_kaggle_data(min_num_reviews, min_rating, min_review_length, min_description_length):
     """
     Load data from the original Kaggle dataset.
     """
@@ -29,7 +29,8 @@ def load_kaggle_data(num_reviews, min_rating, min_review_length, min_description
     average_rating_df = reviews_df.groupby('Title')['review/score'].mean().reset_index()
     average_rating_df = average_rating_df[average_rating_df["review/score"] >= min_rating]
     average_rating_df = average_rating_df.rename(columns = {"review/score": "average_rating"})
-    reviews_df = pd.merge(average_rating_df, reviews_df, on="Title", how="inner")
+    books_df = pd.merge(average_rating_df, books_df, on="Title", how="inner")
+    reviews_df = reviews_df[reviews_df['Title'].isin(books_df['Title'])]
 
     # only keep reviews that are long enough
     reviews_df = reviews_df[reviews_df["review/text"].str.len() >= min_review_length]
@@ -39,31 +40,41 @@ def load_kaggle_data(num_reviews, min_rating, min_review_length, min_description
 
     # want at least num_reviews for a book
     title_counts = reviews_df["Title"].value_counts()
-    title_counts = title_counts[title_counts >= num_reviews]
+    title_counts = title_counts[title_counts >= min_num_reviews]
     enough_reviews_df = pd.DataFrame({"Title":title_counts.index})
     reviews_df = reviews_df[reviews_df['Title'].isin(enough_reviews_df['Title'])]
 
-    # combine all data into a single dataframe
-    books_df = pd.merge(reviews_df, books_df, on="Title", how="inner")
-    return books_df
+    # add the review length as an entry
+    reviews_df["review_length"] = reviews_df["review/text"].apply(lambda x: len(str(x)))
 
-def create_new_subset(num_reviews, min_rating, min_review_length, min_description_length):
+    # update books df for any titles removed from reviews df
+    books_df = books_df[books_df['Title'].isin(reviews_df['Title'])]
+
+    # add the description length as an entry
+    books_df["description_length"] = books_df["description"].apply(lambda x: len(str(x)))
+
+    return books_df, reviews_df
+
+def create_new_subset(min_num_reviews, min_rating, min_review_length, min_description_length):
     folder_path = "./defined_datasets"
     if not os.path.exists(folder_path):
         # Create the folder
         os.makedirs(folder_path)
 
-    books_df = load_kaggle_data(float(num_reviews), float(min_rating), float(min_review_length), float(min_description_length))
+    books_df, reviews_df = load_kaggle_data(float(min_num_reviews), float(min_rating), float(min_review_length), float(min_description_length))
 
     num_books = books_df["Title"].nunique()
-    total_num_reviews = books_df["reviewId"].nunique()
-    file_name = str(num_books) + "_books_" + str(total_num_reviews) + "_total_reviews_" + str(num_reviews) + "_min_reviews_" + str(min_rating) + "_min_rating_" + str(min_description_length) + "_min_description_length_"+ str(min_review_length) + "_min_review_length.csv"
-    file_path = "./defined_datasets/" + file_name
-    books_df.to_csv(file_path, index = False)
+    total_num_reviews = reviews_df["reviewId"].nunique()
+    books_file_name = "books_df_" + str(num_books) + "_books_" + str(min_num_reviews) + "_min_num_reviews_" + str(min_rating) + "_min_rating_" + str(min_description_length) + "_min_description_length_" + str(min_review_length) + "_min_review_length.csv"
+    books_file_path = "./defined_datasets/" + books_file_name
+    reviews_file_name = "reviews_df_" + str(total_num_reviews) + "_total_reviews_" + str(min_num_reviews) + "_min_num_reviews_" + str(min_rating) + "_min_rating_" + str(min_description_length) + "_min_description_length_" + str(min_review_length) + "_min_review_length.csv"
+    reviews_file_path = "./defined_datasets/" + reviews_file_name
+    books_df.to_csv(books_file_path, index = False)
+    reviews_df.to_csv(reviews_file_path, index = False)
 
 if __name__ == "__main__":
-    num_reviews = 30
+    min_num_reviews = 30
     min_rating = 4.2
     min_review_length = 50
     min_description_length = 20
-    create_new_subset(num_reviews, min_rating, min_review_length, min_description_length)
+    create_new_subset(min_num_reviews, min_rating, min_review_length, min_description_length)
