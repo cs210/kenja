27ae58a9001c43cb3609897f3cd3abc98dfe34c0
JosephGuman <joeytg@stanford.edu>
2024-04-17 19:54:53 -0700

    Refactorization of final FAF layer and slight but noticeable optimization to avoid chromadb IO

--- backend/helpers/book_rec_helpers.py
+++ backend/helpers/book_rec_helpers.py
@@ -5,14 +5,14 @@ __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
+from generation_helpers import get_generation
+
 import chromadb
 from chromadb.config import Settings
-import pickle
 from dotenv import load_dotenv
 from openai import OpenAI
 import pandas as pd
 import uuid
-import re
 
 # for RAG embedding model
 import torch
@@ -23,26 +23,16 @@ model = AutoModel.from_pretrained('nomic-ai/nomic-embed-text-v1', trust_remote_c
 model.eval()
 model.to("cuda")
 
+OPTION_COUNT = 5
 
 load_dotenv()
 chroma_client = chromadb.PersistentClient(
     path="./chromadb_data", settings=Settings(anonymized_telemetry=False)
 )
-client = OpenAI()
+temp_client = chromadb.PersistentClient(
+    path="./chromadb_data", settings=Settings(anonymized_telemetry=False)
+)
 
-# Constants for data
-CONFIG_FILE = "config.json"
-EMBEDDING_MODEL = "text-embedding-3-small"
-
-def openai_create_embedding(text, is_document):
-    """
-    Given a description, return an embedding.
-    """
-    value = (client.embeddings.create(input=[text], model=EMBEDDING_MODEL)
-        .data[0]
-        .embedding)
-    type(value)
-    return value
 
 def open_source_create_embeddings(texts_list, is_document):
     """
@@ -229,103 +219,9 @@ def find_match(query):
     extract_dict = middle_collection.get(ids=titles_list, include=["embeddings", "documents", "metadatas"])
     # probably want to be more clever with this name
     client_name = str(uuid.uuid1())
-    extract_collection = chroma_client.create_collection(name=client_name)
+    extract_collection = temp_client.create_collection(name=client_name)
     add_to_collection(extract_collection, extract_dict["embeddings"], extract_dict["documents"], extract_dict["metadatas"], extract_dict["ids"])
-    option_count = 5
-    middle_search_results = embedding_search(extract_collection, query, option_count)
-    chroma_client.delete_collection(name=client_name)
-    
-    '''super_prompt_engineer = (
-f"""Hey ChatGPT. I have a question for you. of the {option_count} words I am hoping for you to provide the top 3 options you think best matches the request/description
-{query}
+    middle_search_results = embedding_search(extract_collection, query, OPTION_COUNT)
+    temp_client.delete_collection(name=client_name)
 
-These are the options\n\n
-""")'''
-
-    '''super_prompt_engineer = 
-    """
-    Pretend you are an expert at books 
-    """'''
-
-    system_prompt = (
-        f"""
-        You are are an expert at reading books. In particular, you have read every book that has ever been published, and have also perused sites like Goodreads.
-        """
-    )
-
-    super_prompt_engineer = (
-        f"""
-        Now, given a request of an ideal book, you will be given {option_count} books and asked to provide the top 3 options. In particular, each of the options will be labeled `Option #`, and will have a corresponding 
-        description and several reviews of the book. After parsing through all of this information, please first explain your reasoning behind your decision-making. After explaining yourself, then return the options,
-        as well as the reasons for choosing each option, in the following format. Please include all of the dashes, and make sure to always include three options:
-
-        Reasoning:
-        <REASONING_FOR_CHOICES>
-
-        Options:
-        - Option #<FIRST_CHOICE>: <TITLE_OF_FIRST_CHOICE>
-        - Reasoning for #<FIRST_CHOICE>: <REASONING_FOR_FIRST_CHOICE>
-        - Option #<SECOND_CHOICE>: <TITLE_OF_SECOND_CHOICE>
-        - Reasoning for #<SECOND_CHOICE>: <REASONING_FOR_SECOND_CHOICE>
-        - Option #<THIRD_CHOICE>
-        - Reasoning for #<THIRD_CHOICE>: <REASONING_FOR_THIRD_CHOICE>
-
-        Each of the options are now below:
-        """
-    )
-
-    book_features = zip(middle_search_results["documents"][0])
-    for i, defining_tuple in enumerate(book_features):
-        super_prompt_engineer += (
-f"""
-Option #{i}:
-{defining_tuple[0]}
-
-
-"""
-        )
-    
-    # Make the call!
-    response = client.chat.completions.create(
-    model="gpt-3.5-turbo-0125",
-    messages=[
-        {"role": "system", "content": system_prompt},
-        {"role": "user", "content": super_prompt_engineer}
-    ]
-    )
-    
-    # Collect all of the data
-    options = []
-    reasonings = []
-    d = {}
-    for i in range(len(middle_search_results["documents"][0])):
-        options.append(f"- Option #{i}")
-        reasonings.append(f"- Reasoning for #{i}")
-        d[f"- Option #{i}"] = middle_search_results["metadatas"][0][i]
-
-    # Get the text
-    text = response.choices[0].message.content
-
-    # Create a regex pattern to search for the options and reasonings
-    pattern = '|'.join(re.escape(option) for option in options)
-    reasoning_pattern = '|'.join(re.escape(reasoning) for reasoning in reasonings)
-
-    # Find all matches in the string, along with their positions
-    matches = [(m.start(), m.group()) for m in re.finditer(pattern, text)]
-    reasoning_matches = [(m.start(), m.group()) for m in re.finditer(reasoning_pattern, text)]
-
-    # Get the last match, if there are any matches
-    last_matches = [i[1] for i in matches][-3:]
-    reasoning_matches = [i for i in reasoning_matches][-3:]
-
-    # Extract the right text
-    for i in range(len(reasoning_matches)):
-        new_text = text[reasoning_matches[i][0]:]
-        next_section = new_text.find("\n")
-        gpt_review = new_text[len("- Reasoning for #1: "):next_section]
-        last_match = last_matches[i]
-        d[last_match]['gpt_review'] = gpt_review
-
-    return [d[last_match] for last_match in last_matches]
-
-
+    return get_generation(middle_search_results, option_count=OPTION_COUNT)
