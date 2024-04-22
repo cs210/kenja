07075b2ece5f3405a312d3bdefb109ced4449ad9
Joey O'Brien <joeyob7@gmail.com>
2024-04-21 18:50:06 -0700

    Lots of work on B2B end-to-end process

--- backend/test_api.py
+++ backend/test_api.py
@@ -8,6 +8,13 @@ from helpers.book_rec_helpers import *
 from typing import List
 import shutil
 
+# Keep track of which files
+# Note: quite hacky to just temporary in-memory DS
+mapping = {
+    'books_dataset': "",
+    'reviews_dataset': ""
+}
+
 # Set up Fast API and allow requests from all sources
 app = FastAPI()
 app.add_middleware(
@@ -20,14 +27,34 @@ app.add_middleware(
 
 @app.post("/upload")
 async def upload_files(files: List[UploadFile] = File(...)):
+    """
+    Upload files to server.
+    """
     try:
         for file in files:
             # Save the file to a location on the server
-            with open("data/" + str(file.filename), "wb") as buffer:
+            with open(file.filename, "wb") as buffer:
                 shutil.copyfileobj(file.file, buffer)
+            
+            # Keep track of correct paths
+            if ("data" in file.filename):
+                mapping["books_dataset"] = file.filename
+            else:
+                mapping["reviews_dataset"] = file.filename
     except Exception as e:
         # Return an error response if something goes wrong
         return JSONResponse(status_code=500, content={"error": str(e)})
-    
+
     # Return a success response
-    return {"message": "Files uploaded successfully"}
+    return {"status": "SUCCESS", "message": "Files uploaded successfully"}
+
+@app.post("/create")
+async def create_embeddings():
+    """
+    Create embeddings under the hood.
+    """
+    try:
+        create_collections(mapping["books_dataset"], mapping["reviews_dataset"])
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
+    return {"status": "SUCCESS", "message": "Embeddings created successfully"}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,8 @@ function App() {
   // State for selected files
   const [selectedFiles, setSelectedFiles] = useState(null);
   const [inPipeline, setInPipeline] = useState(false);
+  const [createEmbeddings, setCreateEmbeddings] = useState(false);
+  const [gotSuccess, setGotSuccess] = useState(false);
 
   /*
    * Set the updated product catalog files.
@@ -38,12 +40,36 @@ function App() {
         body: formData,
       })
       .then(response => {
-        // Handle response
+        // If we got a good response, then we can start creating embeddings!
+        if (response.ok) {
+          setCreateEmbeddings(true);
+          callCreate();
+        }
       })
       .catch(error => {
         // Handle error
       });
     }
+  };
+
+  /*
+   * Upload files to the backend.
+   */
+  const callCreate = () => {
+    // Set embeddings step on and submit to backend
+    setCreateEmbeddings(true);
+    fetch("http://127.0.0.1:8000/create", {
+      method: 'POST',
+    })
+    .then(response => {
+      // If we got a good response, then we can start creating embeddings!
+      if (response.ok) {
+        setGotSuccess(true);
+      }
+    })
+    .catch(error => {
+      // Handle error
+    });
   };
 
   return (
@@ -58,12 +84,24 @@ function App() {
         <input type="file" accept=".txt, .pdf, .doc, .docx" multiple onChange={handleFileChange} />
         <button onClick={handleUpload} className="btn btn-dark">Upload</button>
       </div>
-      { inPipeline === true && <div className="step">
-        <h2>Step 2: Generating Search APIs</h2>
-        <h4>Hang tight: our pipeline is currently generating your search APIs.</h4>
-        <div className="spinner-border text-dark" role="status">
-        </div>
-      </div> }
+      { inPipeline ? <div className="step">
+        <h2>Step 2: Sending Over Data</h2>
+        <h4>We're first sending over your files to be processed.</h4>
+        { createEmbeddings ? <div className="success"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-circle-fill" viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+</svg>  Successfully uploaded!</div> : <div className="spinner-border text-dark" role="status"></div>}
+        </div> : null}
+        { createEmbeddings ? <div className="step">
+        <h2>Step 3: Making Product Data Searchable</h2>
+        <h4>Next, we're making your data usable for our algorithm.</h4>
+        { gotSuccess ? <div className="success"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-circle-fill" viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+</svg>  Successfully created embeddings!</div> : <div className="spinner-border text-dark" role="status"></div>}
+        </div> : null}
+        { gotSuccess ? <div className="step">
+        <h2>Step 4: Use Search APIs</h2>
+        <h4>Congrats! Your data is now searchable -- try calling one of our REST APIs to try it out!</h4>
+        </div> : null}
     </div>
   );
 }
